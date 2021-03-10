048b46574b856efd47496db9cdbfc987d6a504b0
serendatapy <arvaldivia@gmail.com>
2021-03-10 14:56:40 +0000

    mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,38 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: { //similar to data:
+  state: {
     user: { id: 'abc123', name: 'Adam Jahr' },
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
-    events: [
-      { id: 1, title: '...', organizer: '' },
-      { id: 2, title: '...', organizer: '' },
-      { id: 3, title: '...', organizer: '' },
-      { id: 4, title: '...', organizer: '' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
     ],
-    count: 0
+    events: []
   },
   mutations: {
-    INCREMENT_COUNT(state, value) {
-      state.count += value
+    ADD_EVENT(state, event) {
+      state.events.push(event)
     }
-  },//update or mutate state
-  actions: { //recommended you always put mutations inside actions (future proof)
-    updateCount({ state, commit }, value) {//context object, here we destructure it
-      if (state.user) {
-        commit('INCREMENT_COUNT', value)
-      }
-    }
-
   },
-  getters: { //similar to computed state - good for states that require processing
-    getCatCount: state => {
-      return state.categories.length
-    },
-    getEventById: state => id => { //takes state and returns another function that takes id
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
+  getters: {
+    getEventById: state => id => {
       return state.events.find(event => event.id === id)
     }
-  },
-  modules: {}
-});
+  }
+})
