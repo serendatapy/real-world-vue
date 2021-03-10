428726b129300e55cd586c96c62ec5b960798f38
serendatapy <arvaldivia@gmail.com>
2021-03-10 17:19:09 +0000

    feat: mutations p2

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,31 +2,42 @@ import EventShow from './EventShow'
 
 <template>
   <div>
-    <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+      >
+        Prev Page
+      </router-link>
+      |
+    </template>
+    <template v-if="this.$store.state.eventsTotal > this.page * 3">
+      <router-link :to="{ name: 'event-list', query: { page: page + 1 } }">
+        Next Page
+      </router-link>
+    </template>
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue";
-import EventService from "@/services/EventService.js";
+import { mapState } from "vuex";
 export default {
   components: {
     EventCard,
   },
-  data() {
-    return {
-      events: [],
-    };
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1; //page currently on
+    },
+    ...mapState(["events"]),
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data;
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
+    this.$store.dispatch("fetchEvents", {
+      perPage: 3, // <-- How many items to display per page
+      page: this.page, // <-- What page we're on
+    });
   },
 };
 </script>
